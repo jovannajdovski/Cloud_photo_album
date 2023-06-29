7cbe89e9bc9b8e257e131a68d8b2d89133c821d9
uros <urosstanic25@gmail.com>
2023-06-29 02:34:04 +0200

    added conistency for edit

--- photo-album/file/edit/edit_file_s3.py
+++ photo-album/file/edit/edit_file_s3.py
@@ -7,40 +7,49 @@ sns = boto3.client('sns')
 
 
 def lambda_handler(event, context):
-    print(event)
-    message = json.loads(event['Records'][0]['Sns']['Message'])
-
-    print("From SNS: " + str(message))
-
-    file_path = message['file_path']
-    name = message['name']
-
+    messages = event['Records']
     s3 = boto3.resource('s3')
-
+    sqs = boto3.client('sqs')
     bucket_name = os.environ["BucketName"]
+    bucket = s3.Bucket(bucket_name)
 
-    try:
-        bucket = s3.Bucket(bucket_name)
-        objects = bucket.objects.filter(Prefix=file_path)
-        for obj in objects:
-            lastDotIndex = file_path.rfind('.')
-            file_type = file_path[lastDotIndex:]
+    for message in messages:
+        # Process the received message
+        body = json.loads(json.loads(message['body'])['Message'])
 
-            lastSlashIndex = file_path.rfind('/')
-            old_name = file_path[lastSlashIndex + 1:lastDotIndex]
-            if (old_name != name):
-                new_key = file_path[:lastSlashIndex] + '/' + name + file_type
-                bucket.copy({'Bucket': bucket_name, 'Key': obj.key}, new_key)
-                obj.delete()
-                print(f"File {obj.key} modified successfully.")
+        print(body)
+        print("LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGG")
+        # Delete the processed message from the queue
+        receipt_handle = message['receiptHandle']
+        # queue_url = message['eventSourceARN']
+        event_source_arn = message['eventSourceARN']
 
-        return {
-            'statusCode': 200,
-            'body': f'File "{file_path}" is modified successfully.'
-        }
+        file_path = body['file_path']
+        name = body['name']
 
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': f'Error modifying file: {str(e)}'
-        }
+        try:
+            objects = bucket.objects.filter(Prefix=file_path)
+            for obj in objects:
+                lastDotIndex = file_path.rfind('.')
+                file_type = file_path[lastDotIndex:]
+
+                lastSlashIndex = file_path.rfind('/')
+                old_name = file_path[lastSlashIndex + 1:lastDotIndex]
+                if (old_name != name):
+                    new_key = file_path[:lastSlashIndex] + '/' + name + file_type
+                    bucket.copy({'Bucket': bucket_name, 'Key': obj.key}, new_key)
+                    obj.delete()
+                    print(f"File {obj.key} modified successfully.")
+            sqs.delete_message(
+                QueueUrl="https://sqs.eu-central-1.amazonaws.com/205030087586/edit-s3-queue",
+                ReceiptHandle=receipt_handle
+            )
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': f'Error modifying file: {str(e)}'
+            }
+    return {
+        'statusCode': 200,
+        'body': f'File "{file_path}" is modified successfully.'
+    }
--- photo-album/file/edit/edit_metadata_ddb.py
+++ photo-album/file/edit/edit_metadata_ddb.py
@@ -1,77 +1,79 @@
 import json
 import os
-
+import re
 import boto3
 from datetime import datetime
 
 
 def lambda_handler(event, context):
-    print(event)
-    message = json.loads(event['Records'][0]['Sns']['Message'])
-    print("From SNS: " + str(message))
-
+    messages = event['Records']
+    sqs = boto3.client('sqs')
     dynamodb_client = boto3.resource('dynamodb')
     table = dynamodb_client.Table(os.environ["TableName"])
-    print(table)
-    bucket_name = os.environ["BucketName"]
-    print(bucket_name)
-    file_path = message['file_path']
-    item_id = bucket_name + "/" + file_path
 
-    new_name = message['name']
-    new_tag = message['tag']
-    new_description = message['description']
+    for message in messages:
 
-    lastDotIndex = file_path.rfind('.')
-    file_type = file_path[lastDotIndex:]
-    lastSlashIndex = file_path.rfind('/')
+        print("LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGG")
+        print(message)
 
-    new_id = bucket_name + '/' + file_path[:lastSlashIndex] + '/' + new_name + file_type
-    print("dosao do try ", new_id, item_id)
-    try:
-        response = table.get_item(Key={'id': item_id})
-        print(response)
-        item = response.get('Item')
-        if item:
-            item['id'] = new_id
-            item['name'] = new_name
-            item['tag'] = new_tag
-            item['description'] = new_description
-            item['editTime'] = int(datetime.now().timestamp())
-            '''print('new item ',item)
-            table.update_item(
-                Key={'id': item_id},
-                UpdateExpression='SET id = :new_id, #nm = :new_name, tag = :new_tag, description = :new_description, editTime = :new_edit_time',
-                ExpressionAttributeValues={
-                    ':new_id': item['id'],
-                    ':new_name': item['name'],
-                    ':new_tag': item['tag'],
-                    ':new_description': item['description'],
-                    ':new_edit_time': item['editTime']
-                },
-                ExpressionAttributeNames={
-                    '#nm': 'name'
+        body = json.loads(json.loads(message['body'])['Message'])
+
+        print(body)
+        print("LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGG")
+        # Delete the processed message from the queue
+        receipt_handle = message['receiptHandle']
+        # queue_url = message['eventSourceARN']
+        event_source_arn = message['eventSourceARN']
+
+        bucket_name = os.environ["BucketName"]
+
+        file_path = body['file_path']
+        item_id = bucket_name + "/" + file_path
+
+        new_name = body['name']
+        new_tag = body['tag']
+        new_description = body['description']
+
+        lastDotIndex = file_path.rfind('.')
+        file_type = file_path[lastDotIndex:]
+        lastSlashIndex = file_path.rfind('/')
+
+        new_id = bucket_name + '/' + file_path[:lastSlashIndex] + '/' + new_name + file_type
+        print("dosao do try ", new_id, item_id)
+        try:
+            response = table.get_item(Key={'id': item_id})
+            print(response)
+            item = response.get('Item')
+            if item:
+                item['id'] = new_id
+                item['name'] = new_name
+                item['tag'] = new_tag
+                item['description'] = new_description
+                item['editTime'] = int(datetime.now().timestamp())
+
+                print(item_id)
+                table.delete_item(Key={'id': item_id})
+                print(item)
+
+                response = table.put_item(
+                    Item=item
+                )
+                print('zavrsio edit')
+                sqs.delete_message(
+                    QueueUrl="https://sqs.eu-central-1.amazonaws.com/205030087586/edit-ddb-queue",
+                    ReceiptHandle=receipt_handle
+                )
+            else:
+                return {
+                    'statusCode': 404,
+                    'body': f'File not found: {str(e)}'
                 }
-            )'''
-            print(item_id)
-            table.delete_item(Key={'id': item_id})
-            print(item)
-
-            response = table.put_item(
-                Item=item
-            )
-            print('zavrsio edit')
+        except Exception as e:
             return {
-                'statusCode': 200,
-                'body': f'File "{file_path}" is modified successfully.'
+                'statusCode': 500,
+                'body': f'Error modifying file: {str(e)}'
             }
-        else:
-            return {
-                'statusCode': 404,
-                'body': f'File not found: {str(e)}'
-            }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': f'Error modifying file: {str(e)}'
-        }
+    return {
+        'statusCode': 200,
+        'body': f'File "{file_path}" is modified successfully.'
+    }
